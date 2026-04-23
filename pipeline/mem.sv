d30df6cbd53bb5b52431a2ced4c68b927ace0c7a
roomroofroot <93904861+roomroofroot@users.noreply.github.com>
2026-04-23 13:53:18 +0530

    test acc8

--- pipeline/mem.sv
+++ pipeline/mem.sv
@@ -5,7 +5,7 @@ module imem(
   logic [31:0] RAM[63:0];
 
   initial
-    $readmemh("test.asc", RAM, 0, 20);
+    $readmemh("test-simd.asc", RAM, 0, 4);
 
   assign rd = RAM[a[31:2]]; // word aligned
 endmodule
